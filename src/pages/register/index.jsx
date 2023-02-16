e2fa2871a6fc7e8e22a6666960736923ef035d80
Leo <leo.coyere94@gmail.com>
2023-02-16 11:26:54 +0100

    implement BEM classes

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -2,21 +2,21 @@ import { Link } from "react-router-dom"
 
 export default function Register() {
     return (
-        <main className="form-wrapper">
+        <main className="page-wrapper">
             <form action="" className="form">
-                <nav className="form-nav">
-                    <Link to='/login' className="form-link">Login</Link>
-                    <Link to='/register' className="form-link form-link--red">Create an account</Link>
+                <nav className="form__nav">
+                    <Link to='/login' className="form__link">Login</Link>
+                    <Link to='/register' className="form__link form__link--red">Create an account</Link>
                 </nav>
-                <div className="form-row">
-                    <input type="text" className="form-input" placeholder="First name"/>
-                    <input type="text" className="form-input" placeholder="Last name"/>
+                <div className="form__row">
+                    <input type="text" className="form__input" placeholder="First name"/>
+                    <input type="text" className="form__input" placeholder="Last name"/>
                 </div>
-                <input type="email" className="form-input" placeholder="Mail adress"/>
-                <input type="email" className="form-input" placeholder="Confirm mail adress"/>
-                <input type="password" className="form-input" placeholder="Password"/>
-                <input type="password" className="form-input" placeholder="Confirm password"/>
-                <button type="submit" className="form-button form-button--red">Create my account</button>
+                <input type="email" className="form__input" placeholder="Mail adress"/>
+                <input type="email" className="form__input" placeholder="Confirm mail adress"/>
+                <input type="password" className="form__input" placeholder="Password"/>
+                <input type="password" className="form__input" placeholder="Confirm password"/>
+                <button type="submit" className="form__button form__button--red">Create my account</button>
             </form>
         </main>
     )
